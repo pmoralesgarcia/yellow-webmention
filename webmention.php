6fba853804d2772358cc3ee56a00380e3a72c486
Pablo Morales <33302229+pmoralesgarcia@users.noreply.github.com>
2023-06-28 00:29:29 -0700

    Update webmention.php
    changes

--- webmention.php
+++ webmention.php
@@ -4,3 +4,20 @@
 class YellowWebmention {
     const VERSION = "0.1.0";
     public $yellow;         //access to API
+
+     // Handle initialisation
+    public function onLoad($yellow) {
+    $this->yellow = $yellow;
+        $this->yellow->system->setDefault("webmentionModerator", "");
+        $this->yellow->system->setDefault("webmentionDirectory", "webmention/");
+        $this->yellow->system->setDefault("webmentionAutoPublish", "0");
+        $this->yellow->system->setDefault("webmentionMaxSize", "5000");
+        $this->yellow->system->setDefault("webmentionTimeout", "0");
+        $this->yellow->system->setDefault("webmentionOpening", "30");
+        $this->yellow->system->setDefault("webmentionAuthorNotification", "1");
+        $this->yellow->system->setDefault("webmentionSpamFilter", "href=|url=");
+        $this->yellow->system->setDefault("webmentionIconSize", "80");
+        $this->yellow->system->setDefault("webmentionProfileIcon", "0");
+        $this->yellow->system->setDefault("webmentionProfileIconDefault", "mp");
+        $this->yellow->system->setDefault("webmentionConsent", "0");
+        $this->yellow->language->setDefaults([
