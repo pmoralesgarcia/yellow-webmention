c6b2a0497d736e561c8db3cbfefc990a436872d8
Pablo Morales <pm8morales@gmail.com>
2024-03-17 21:01:55 -0700

    fix code

--- webmention.js
+++ webmention.js
@@ -1,104 +1,103 @@
-var target_url = window.location.href.toString().replace(/\/edit/, "");
-var target_url1 = "https://www.disquisitioner.com/posts/post-darkmode/";
+const targetUrl = window.location.href.toString().replace(/\/edit/, "");
 
 // jsonp=f option to disable json return
-var count_url   = "https://webmention.io/api/count.json?target=";
-var mention_url = "https://webmention.io/api/mentions.jf2?target=";
-var mfilter = "&wm-property=in-reply-to";
+const countUrl   = "https://webmention.io/api/count.json?target=";
+const mentionentionUrl = "https://webmention.io/api/mentions.jf2?target=";
+const mentionfilter = "&wm-property=in-reply-to";
 
-var defaultphoto = "https://aaronparecki.com/assets/images/no-profile-photo.png"
+const defaultPhoto = "https://aaronparecki.com/assets/images/no-profile-photo.png"
 
-fetch(count_url+target_url)
+fetch(countUrl+targetUrl)
   .then(response => {return(response.text());})
   .then(myJson => {showCounts(1,JSON.parse(myJson));});
 
-fetch(mention_url+target_url)
+fetch(mentionUrl+targetUrl)
   .then(response => {return(response.text());})
   .then(myJson => {showMentions(JSON.parse(myJson));});
 
 function showCounts(index, data) {
   // Add code to hide mention panel if data.count = 0
-  if(data.type.like) { document.getElementById("wm_like"+index).innerHTML = data.type.like; }
-  if(data.type.mention) { document.getElementById("wm_ment"+index).innerHTML = data.type.mention; }
-  if(data.type.reply) {
+  if (data.type.like) { document.getElementById("webmentionLike"+index).innerHTML = data.type.like; }
+  if (data.type.mention) { document.getElementById("webmentionMention"+index).innerHTML = data.type.mention; }
+  if (data.type.reply) {
       document.getElementById("wm_reply"+index).innerHTML =
         pluralize(data.type.reply,"reply","replies");
   }
-  if(data.type.repost) {
+  if (data.type.repost) {
     document.getElementById("wm_repost"+index).innerHTML =
       pluralize(data.type.repost,"repost","reposts");
   }
-  if(data.type.bookmark) { document.getElementById("wm_bkmk"+index).innerHTML = data.type.bookmark; }
+  if (data.type.bookmark) { document.getElementById("wm_bkmk"+index).innerHTML = data.type.bookmark; }
 }
 
 function pluralize(num,singular,plural) {
-  if(num == 1) return num + " " + singular;
+  if (num == 1) return num + " " + singular;
   return num + " " + plural;
 }
 
 function showMentions(mentions) {
-  var panel = document.getElementById("mentionpanel");
+  const panel = document.getElementById("mentionpanel");
   for(i=0;i<mentions.children.length;i++) {
 
-    var m = mentions.children[i];
+    const mention = mentions.children[i];
 
-    var m_div = document.createElement("DIV");
-    m_div.className = "webmention";
-    panel.appendChild(m_div);
+    const mentionMentionDiv = document.createElement("div");
+    mentionDiv.className = "webmention";
+    panel.appendChild(mentionDiv);
 
-    var m_img = document.createElement("IMG");
-    if(m.author.photo) {
-      m_img.setAttribute("src",m.author.photo);
+    const mentionentionImg = document.createElement("div");
+    if (m.author.photo) {
+      mentionImg.setAttribute("src",m.author.photo);
     }
     else {
-      m_img.setAttribute("src",defaultphoto);
+      mentionImg.setAttribute("src",defaultPhoto);
     }
-    m_img.setAttribute("width","64");
-    m_div.appendChild(m_img);
+    mentionImg.setAttribute("width","64");
+    mentionDiv.appendChild(mentionImg);
 
-    var m_info = document.createElement("DIV");
-    m_info.className = "wm_info";
-    m_div.appendChild(m_info);
+    const mentionentionInfo = document.createElement("div");
+    mentionInfo.className = "wmentionInfo";
+    mentionDiv.appendChild(mentionInfo);
 
     // First line is author's name, liked to author's web site if present
-    if(m.author.url) {
-      var m_auth = document.createElement("A");
+    if (m.author.url) {
+      const mention_auth = document.createElement("a");
       m_auth.href = m.author.url;
-      if(m.author.name) {
+      if (m.author.name) {
         m_auth.innerHTML = m.author.name;
       }
       m_auth.className = "m_author";
-      m_info.appendChild(m_auth);
+      mentionInfo.appendChild(m_auth);
 
     }
     else {
-      if(m.author.name) {
-        var m_auth = document.createElement("SPAN");
+      if (m.author.name) {
+        const mention_auth = document.createElement("span");
         m_auth.innerHTML = m.author.name;
         m_auth.className = "m_author";
-        m_info.appendChild(m_auth);
+        mentionInfo.appendChild(m_auth);
       }
     }
 
     // Second line = name of post linked to post URL, though
     // if no name then is just URL
-    var m_art = document.createElement("A");
+    const mention_art = document.createElement("a");
     m_art.href = m.url;
-    if(m.name) {
+    if (m.name) {
       m_art.innerHTML = m.name;
     }
     else {
       m_art.innerHTML = m.url;
     }
-    m_info.appendChild(m_art);
+    mentionInfo.appendChild(m_art);
 
-    if(m.published) {
-      var d = new Date(m.published);
-      var m_pub = document.createElement("TIME");
-      m_pub.datetime = m.published;
-      m_pub.innerHTML = "Published: " + d.toString();
-      m_pub.className = "m_published";
-      m_info.appendChild(m_pub);
+    if (m.published) {
+      const date = new Date(m.published);
+      const mentionentionPub = document.createElement("TIME");
+      mentionPub.datetime = m.published;
+      mentionPub.innerHTML = "Published: " + d.toString();
+      mentionPub.className = "mentionPublished";
+      mentionInfo.appendChild(mentionPub);
     }
   }
 }
