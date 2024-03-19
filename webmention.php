c7696bcb6f307ca4465f7546e6e550b263306a19
Robert Pfotenhauer <ratterobert@gmail.com>
2024-03-19 11:10:27 +0100

    tidied up a bit
    ## extension.ini
    - removed icon.css
    
    ## icon.css
    - removed icon.css because of yellow extension of the same name
    
    ## webmention.css
    - added icon-styles to webmention.css
    - `.micon` simplified
    
    ## webmention.php
    - addded system setting `webmentionEndpoint`
    - added ability to use `yellow-icon` extension
    - added if-statement for webmention to `onParsePageExtra()`
    - added `<link rel="webmention" ..>` to `onParsePageExtra()`
    - removed `&nbsp;`
    - removed icon.css from `onParsePageExtra()`

--- webmention.php
+++ webmention.php
@@ -6,44 +6,56 @@ class YellowWebmention {
     // Handle initialisation
     public function onLoad($yellow) {
         $this->yellow = $yellow;
+        $this->yellow->system->setDefault("webmentionEndpoint", "PUT_YOUR_WEBMENTION_ENDPOINT_HERE");
     }
 
     // Handle page content of shortcut
     public function onParseContentShortcut($page, $name, $text, $type) {
         $output = null;
         if ($name=="webmention" && ($type=="block" || $type=="inline")) {
-            $output = '<div class="webmention">';
-            $output .= '<ul class="menicons">';
-            $output .= '<ul class="menicons">';
-            $output .= '<li class="micon"><i class="material-icons">star_border</i><span id="wm_like1"></span>&nbsp;</li>';
-            $output .= '<li class="micon"><i class="material-icons-outlined">description</i><span id="wm_ment1"></span>&nbsp;</li>';
-            $output .= '';
-            $output .= '<li class="micon"><i class="material-icons">chat_bubble_outline</i><span id="wm_reply1"></span></li>';
-            $output .= '';
-            $output .= '<li class="micon"><i class="material-icons">repeat</i><span id="wm_repost1"></span></li>';
-            $output .= '';
-            $output .= '<li class="micon"><i class="material-icons">bookmark_border</i><span id="wm_bkmk1"></span>&nbsp;</li>';
-            $output .= '';
-            $output .= '</div>';
-            $output .= '<hr>';
-            $output .= '<div id="mentionpanel"></div>';
-            $output .= '</div>';
-
-            
+            if ($this->yellow->extension->isExisting("icon")) {
+                $output = "<div class=\"webmention\">";
+                $output .= "<ul class=\"menicons\">";
+                $output .= "<li class=\"micon\"><i class=\"icon fa-star-o\"></i><span id=\"wm_like1\"></span></li>";
+                $output .= "<li class=\"micon\"><i class=\"icon fa-file-text-o\"></i><span id=\"wm_ment\"></span></li>";
+                $output .= "<li class=\"micon\"><i class=\"icon fa-comment-o\"></i><span id=\"wm_reply\"></span></li>";
+                $output .= "<li class=\"micon\"><i class=\"icon fa-refresh\"></i><span id=\"wm_repost\"></span></li>";
+                $output .= "<li class=\"micon\"><i class=\"icon fa-bookmark-o\"></i><span id=\"wm_bkmk\"></span></li>";
+                $output .= "</ul>";
+                $output .= "</div>";
+                $output .= "<hr>";
+                $output .= "<div id=\"mentionpanel\"></div>";
+                $output .= "</div>";
+            } else {
+                $output = "<div class=\"webmention\">";
+                $output .= "<ul class=\"menicons\">";
+                $output .= "<li class=\"micon\"><i class=\"material-icons\">star_border</i><span id=\"wm_like1\"></span></li>";
+                $output .= "<li class=\"micon\"><i class=\"material-icons-outlined\">description</i><span id=\"wm_ment\"></span></li>";
+                $output .= "<li class=\"micon\"><i class=\"material-icons\">chat_bubble_outline</i><span id=\"wm_reply\"></span></li>";
+                $output .= "<li class=\"micon\"><i class=\"material-icons\">repeat</i><span id=\"wm_repost\"></span></li>";
+                $output .= "<li class=\"micon\"><i class=\"material-icons\">bookmark_border</i><span id=\"wm_bkmk\"></span></li>";
+                $output .= "</ul>";
+                $output .= "</div>";
+                $output .= "<hr>";
+                $output .= "<div id=\"mentionpanel\"></div>";
+                $output .= "</div>";
+            }
         }
         return $output;
     }
 
-        // Handle page extra data
-        public function onParsePageExtra($page, $name) {
-            $output = null;
-            if ($name=="header") {
-                $extensionLocation = $this->yellow->system->get("coreServerBase").$this->yellow->system->get("coreExtensionLocation");
-                $output = "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$extensionLocation}webmention.css\" />\n";
-                $output .= "<script async type=\"text/javascript\" defer=\"defer\" src=\"{$extensionLocation}webmention.js\"></script>\n";
-                $output .= "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$extensionLocation}icon.css\" />\n";
-
-            }
-            return $output;
+    // Handle page extra data
+    public function onParsePageExtra($page, $name) {
+        $output = null;
+        if ($name=="webmention") { // So that it is displayed with `echo $this->yellow->page->getExtraHtml("webmention")`
+            $output .= $this->onParseContentShortcut($page, "webmention", "", "block");
         }
+        if ($name=="header") {
+            $extensionLocation = $this->yellow->system->get("coreServerBase").$this->yellow->system->get("coreExtensionLocation");
+            $output .= "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$extensionLocation}webmention.css\">\n";
+            $output .= "<link rel=\"webmention\" href=\"". $this->yellow->system->get("webmentionEndpoint") ."\">\n";
+            $output .= "<script async type=\"text/javascript\" defer=\"defer\" src=\"{$extensionLocation}webmention.js\"></script>\n";
+        }
+        return $output;
+    }
 }
